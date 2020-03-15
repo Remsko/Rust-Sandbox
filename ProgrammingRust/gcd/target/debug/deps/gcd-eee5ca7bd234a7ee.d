4823ccaa7334563d1831794b57a496ed90749c07
Remsko <pinoit.remi@gmail.com>
2020-03-15 15:23:39 +0100

    cargo clean gcd

--- ProgrammingRust/gcd/target/debug/deps/gcd-eee5ca7bd234a7ee.d
+++
@@ -1,5 +0,0 @@
-/home/rpinoit/Desktop/Workspace/Rust-Sandbox/ProgrammingRust/gcd/target/debug/deps/gcd-eee5ca7bd234a7ee: src/main.rs
-
-/home/rpinoit/Desktop/Workspace/Rust-Sandbox/ProgrammingRust/gcd/target/debug/deps/gcd-eee5ca7bd234a7ee.d: src/main.rs
-
-src/main.rs:
--- ProgrammingRust/gcd/target/debug/gcd.d
+++
@@ -1,1 +0,0 @@
-/home/rpinoit/Desktop/Workspace/Rust-Sandbox/ProgrammingRust/gcd/target/debug/gcd: /home/rpinoit/Desktop/Workspace/Rust-Sandbox/ProgrammingRust/gcd/src/main.rs
