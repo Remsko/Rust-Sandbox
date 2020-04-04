a38ef1b906de6cb422f2732813eaa6aafe1460e0
Remsko <pinoit.remi@gmail.com>
2020-04-05 00:39:41 +0200

    work on tcp

--- TCP/src/main.rs
+++ TCP/src/main.rs
@@ -1,24 +1,58 @@
+use std::collections::HashMap;
 use std::io;
+use std::net::Ipv4Addr;
+
+mod tcp;
+
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+struct Quad {
+	src: (Ipv4Addr, u16),
+	dst: (Ipv4Addr, u16),
+}
 
 fn main() -> io::Result<()> {
-	let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+	let mut connections: HashMap<Quad, tcp::State> = Default::default();
+	let mut nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
 	let mut buf = [0u8; 1504];
 
 	loop {
 		let nbytes = nic.recv(&mut buf[..])?;
-		let flags = u16::from_be_bytes([buf[0], buf[1]]);
-		let proto = u16::from_be_bytes([buf[2], buf[3]]);
+		let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+		let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
 
-		if proto != 0x0800 {
+		if eth_proto != 0x0800 {
 			continue;
 		}
-		println!(
-			"read {} bytes: (flags: {:x}, proto: {:x}): {:x?}",
-			nbytes - 4,
-			flags,
-			proto,
-			&buf[4..nbytes]
-		);
+
+		match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+			Ok(iph) => {
+				let src = iph.source_addr();
+				let dst = iph.destination_addr();
+				let proto = iph.protocol();
+
+				if proto != 0x06 {
+					continue;
+				}
+
+				match etherparse::TcpHeaderSlice::from_slice(&buf[4 + iph.slice().len()..nbytes]) {
+					Ok(tcph) => {
+						let datai = 4 + iph.slice().len() + tcph.slice().len();
+						connections
+							.entry(Quad {
+								src: (src, tcph.source_port()),
+								dst: (dst, tcph.destination_port()),
+							})
+							.or_default()
+							.on_packet(&mut nic, iph, tcph, &buf[datai..nbytes])?;
+					}
+					Err(e) => {
+						eprintln!("ignoring weird tcp packet {:?}", e);
+					}
+				}
+			}
+			Err(e) => {
+				eprintln!("ignoring weird packet {:?}", e);
+			}
+		}
 	}
-	Ok(())
 }
